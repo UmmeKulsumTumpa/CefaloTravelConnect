e8f0a86ccefa6459217769484bca58213b0a3ebc
Umme Kulsum Tumpa <bsse1307@iit.du.ac.bd>
2025-06-02 00:32:11 +0600

    feat: add knex configurations, users schema and route testing

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,9 +9,5 @@ app.use(cors());
 // application routes
 app.use('/api/v1', router);
 
-app.use("/", async (req: Request, res: Response) => {
-  res.send("app is running successfully");
-});
-
 export default app;
 
